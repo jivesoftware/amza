e7090f88c512a572b094136b3d1959f5ab7fcc1e
Pete Matern <pete@jivesoftware.com>
2013-11-16 22:58:58 -0800

    still fixing git sadness

--- code/com/jivesoftware/os/amza/amza-transport/src/main/java/com/jivesoftware/os/amza/transport/tcp/replication/client/TcpChangeSetTaker.java
+++ code/com/jivesoftware/os/amza/amza-transport/src/main/java/com/jivesoftware/os/amza/transport/tcp/replication/client/TcpChangeSetTaker.java
@@ -7,30 +7,42 @@ import com.jivesoftware.os.amza.shared.TransactionSet;
 import com.jivesoftware.os.amza.shared.TransactionSetStream;
 import com.jivesoftware.os.amza.transport.tcp.replication.messages.ChangeSetRequest;
 import com.jivesoftware.os.amza.transport.tcp.replication.messages.ChangeSetResponse;
-import com.jivesoftware.os.amza.transport.tcp.replication.shared.TcpClient;
-import com.jivesoftware.os.amza.transport.tcp.replication.shared.TcpClientProvider;
+import com.jivesoftware.os.amza.transport.tcp.replication.shared.BufferProvider;
+import com.jivesoftware.os.amza.transport.tcp.replication.shared.MessageFramer;
+import com.jivesoftware.os.amza.transport.tcp.replication.shared.SendReceiveChannel;
+import com.jivesoftware.os.amza.transport.tcp.replication.shared.SendReceiveChannelProvider;
 import com.jivesoftware.os.jive.utils.base.interfaces.CallbackStream;
 import com.jivesoftware.os.jive.utils.logger.MetricLogger;
 import com.jivesoftware.os.jive.utils.logger.MetricLoggerFactory;
+import java.nio.ByteBuffer;
 
 /**
  *
  */
-public class TcpChangeSetTaker implements ChangeSetTaker {
+public class TCPChangeSetTaker implements ChangeSetTaker {
 
     private static final MetricLogger LOG = MetricLoggerFactory.getLogger();
-    private final TcpClientProvider tcpClientProvider;
+    private final SendReceiveChannelProvider clientChannelProvider;
+    private final MessageFramer messageFramer;
+    private final BufferProvider bufferProvider;
 
-    public TcpChangeSetTaker(TcpClientProvider tcpClientProvider) {
-        this.tcpClientProvider = tcpClientProvider;
+    public TCPChangeSetTaker(SendReceiveChannelProvider clientChannelProvider, MessageFramer messageFramer, BufferProvider bufferProvider) {
+        this.clientChannelProvider = clientChannelProvider;
+        this.messageFramer = messageFramer;
+        this.bufferProvider = bufferProvider;
     }
 
     @Override
     public synchronized <K, V> void take(RingHost ringHost, TableName<K, V> tableName, long transationId,
         final TransactionSetStream transactionSetStream) throws Exception {
-        TcpClient client = tcpClientProvider.getClientForHost(ringHost);
+        SendReceiveChannel channel = clientChannelProvider.getChannelForHost(ringHost);
+
+        ByteBuffer sendBuff = bufferProvider.acquire();
         try {
-            client.sendMessage(new ChangeSetRequest());
+            messageFramer.toFrame(new ChangeSetRequest(), sendBuff);
+            channel.send(sendBuff);
+
+            sendBuff.clear();
 
             CallbackStream<TransactionSet> messageStream = new CallbackStream<TransactionSet>() {
                 @Override
@@ -46,7 +58,7 @@ public class TcpChangeSetTaker implements ChangeSetTaker {
             ChangeSetResponse entry = null;
             boolean streamingResults = true;
 
-            while ((entry = client.receiveMessage(ChangeSetResponse.class)) != null) {
+            while ((entry = readChangeSetResponse(channel, sendBuff)) != null) {
 
                 //if we aren't dispatching results anymore, we still need to loop over the input to drain the socket
                 if (streamingResults) {
@@ -64,9 +76,23 @@ public class TcpChangeSetTaker implements ChangeSetTaker {
                     break;
                 }
             }
+
         } finally {
-            tcpClientProvider.returnClient(client);
+            bufferProvider.release(sendBuff);
+        }
+    }
+
+    private ChangeSetResponse readChangeSetResponse(SendReceiveChannel sendReceiveChannel, ByteBuffer readBuffer) throws Exception {
+        ChangeSetResponse response = null;
+        int read = 0;
+        int lastRead = 0;
+
+        while (response == null && lastRead >= 0) {
+            lastRead = sendReceiveChannel.receive(readBuffer);
+            read += lastRead;
+            response = messageFramer.<ChangeSetResponse>fromFrame(readBuffer, read, ChangeSetResponse.class);
         }
 
+        return response;
     }
 }

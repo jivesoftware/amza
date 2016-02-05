51b36bb6d7c294f32d00fb5ddf6a02eb5ac77df8
Jonathan Colt <jonathan.colt@jivesoftware.com>
2016-02-04 20:50:10 -1000

    upgrade to lab 0.16

---
+++ amza-lab-storage/src/main/java/com/jivesoftware/os/amza/lab/pointers/LABPointerIndexConfig.java
@@ -0,0 +1,31 @@
+package com.jivesoftware.os.amza.deployable;
+
+import org.merlin.config.Config;
+import org.merlin.config.defaults.IntDefault;
+import org.merlin.config.defaults.LongDefault;
+import org.merlin.config.defaults.StringDefault;
+
+public interface AmzaConfig extends Config {
+
+    @StringDefault("./var/data")
+    String getWorkingDirs();
+
+    @IntDefault(1000)
+    int getTakeFromNeighborsIntervalInMillis();
+
+    @LongDefault(60_000)
+    long getCheckIfCompactionIsNeededIntervalInMillis();
+
+    @IntDefault(8)
+    int getNumberOfCompactorThreads();
+
+    @IntDefault(8)
+    int getNumberOfTakerThreads();
+
+    @LongDefault(30_000L)
+    long getDiscoveryIntervalMillis();
+
+    @LongDefault(1_000L)
+    long getAsyncFsyncIntervalMillis();
+
+}

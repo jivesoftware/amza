fb81dc4c8c54ee228b3be2aa8133f34bb0446b60
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2016-05-04 23:26:30 -0500

    fix prefixUpperExclusive

---
+++ amza-api/src/test/java/com/jivesoftware/os/amza/api/wal/WALKeyTest.java
@@ -0,0 +1,25 @@
+package com.jivesoftware.os.amza.api.wal;
+
+import java.util.Arrays;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ */
+public class WALKeyTest {
+
+    @Test
+    public void testPrefixUpperExclusive() throws Exception {
+
+        for (int i = Byte.MIN_VALUE; i <= Byte.MAX_VALUE; i++) {
+            byte[] lower = new byte[] { 0, 0, 0, (byte) i };
+            byte[] upper = WALKey.prefixUpperExclusive(lower);
+            int c = KeyUtil.compare(lower, upper);
+            Assert.assertTrue(c < 0, Arrays.toString(lower) + " vs " + Arrays.toString(upper) + " = " + c);
+        }
+
+    }
+}

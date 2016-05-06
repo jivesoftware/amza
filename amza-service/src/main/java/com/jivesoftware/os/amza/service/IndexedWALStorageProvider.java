1ef963c3727d75990ec41e7a17ab8820780f400d
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2016-05-06 18:48:58 -0500

    hash lab indexes into modulo dirs; repair wal modulo hash

--- amza-service/src/main/java/com/jivesoftware/os/amza/service/IndexedWALStorageProvider.java
+++ amza-service/src/main/java/com/jivesoftware/os/amza/service/IndexedWALStorageProvider.java
@@ -11,8 +11,11 @@ import com.jivesoftware.os.amza.service.storage.binary.BinaryHighwaterRowMarshal
 import com.jivesoftware.os.amza.service.storage.binary.BinaryWALTx;
 import com.jivesoftware.os.amza.service.storage.binary.RowIOProvider;
 import com.jivesoftware.os.jive.utils.ordered.id.TimestampedOrderIdProvider;
+import com.jivesoftware.os.mlogger.core.MetricLogger;
+import com.jivesoftware.os.mlogger.core.MetricLoggerFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Random;
 import org.apache.commons.io.FileUtils;
 
@@ -20,6 +23,8 @@ import org.apache.commons.io.FileUtils;
  * @author jonathan.colt
  */
 public class IndexedWALStorageProvider {
+
+    private static final MetricLogger LOG = MetricLoggerFactory.getLogger();
 
     private final Random rand = new Random();
     private final AmzaStats amzaStats;
@@ -42,7 +47,7 @@ public class IndexedWALStorageProvider {
         TimestampedOrderIdProvider orderIdProvider,
         SickPartitions sickPartitions,
         int tombstoneCompactionFactor,
-        long rebalanceIfImbalanceGreaterThanInBytes) {
+        long rebalanceIfImbalanceGreaterThanInBytes) throws IOException {
         this.amzaStats = amzaStats;
 
         this.workingDirectories = workingDirectories;
@@ -54,6 +59,67 @@ public class IndexedWALStorageProvider {
         this.sickPartitions = sickPartitions;
         this.tombstoneCompactionFactor = tombstoneCompactionFactor;
         this.rebalanceIfImbalanceGreaterThanInBytes = rebalanceIfImbalanceGreaterThanInBytes;
+
+        for (File workingDirectory : workingDirectories) {
+            if (workingDirectory.exists()) {
+                for (File file : FileUtils.listFiles(workingDirectory, new String[] { "kvt" }, true)) {
+                    File dest = convert(workingDirectory, file);
+                    if (dest != null) {
+                        if (!dest.getParentFile().mkdirs()) {
+                            throw new IOException("Failed to mkdirs for " + dest);
+                        }
+                        if (!file.renameTo(dest)) {
+                            throw new IOException("Failed to rename " + file + " to " + dest);
+                        }
+                        LOG.info("We repaired WAL from {} to {}", file, dest);
+                    }
+                }
+            }
+        }
+    }
+
+    private static File convert(File workingDirectory, File file) {
+        String workingPath = workingDirectory.getAbsolutePath();
+        String filename = file.getName();
+        String partition = filename.substring(0, filename.indexOf('.'));
+        try {
+            long partitionVersion = Long.parseLong(partition); // 12345
+            String trailingPath = file.getAbsolutePath().substring(workingPath.length() + 1); // 0/v15/backup/12345
+            int firstSlash = trailingPath.indexOf('/'); // 1
+            int currentModulo = Integer.parseInt(trailingPath.substring(0, firstSlash)); // 0
+            String partitionPath = trailingPath.substring(firstSlash); // /v15/backup/12345
+            long actualModulo = hash(partitionVersion) % 1024;
+            if (currentModulo != actualModulo) {
+                return new File(workingDirectory + "/" + actualModulo + partitionPath);
+            } else {
+                LOG.info("Skipped repair for WAL {}", partition);
+            }
+        } catch (NumberFormatException e) {
+            LOG.info("Did not repair system WAL {}", partition);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        File from = new File("/example/barf/0/v15/backup/12345.kvt");
+        File dest = convert(new File("/example/barf"), from);
+        System.out.println(from.getAbsolutePath() + " to " + (dest == null ? "null" : dest.getAbsolutePath()));
+
+        dest = convert(new File("/example/barf/"), from);
+        System.out.println(from.getAbsolutePath() + " to " + (dest == null ? "null" : dest.getAbsolutePath()));
+
+        from = new File("/example/barf/0/v15/backup/AAAAAAA==.kvt");
+        dest = convert(new File("/example/barf/"), from);
+        System.out.println(from.getAbsolutePath() + " to " + (dest == null ? "null" : dest.getAbsolutePath()));
+
+        int[] count = new int[1024];
+        for (int i = 0; i < 10_000; i += 2) {
+            int h = (int) (hash((long) i) % 1024);
+            count[h]++;
+        }
+        for (int i = 0; i < 1024; i++) {
+            System.out.println(i + ". " + count[i]);
+        }
     }
 
     private String name(VersionedPartitionName versionedPartitionName) throws IOException {
@@ -103,7 +169,29 @@ public class IndexedWALStorageProvider {
     }
 
     public File baseKey(VersionedPartitionName versionedPartitionName, int stripe) {
-        return new File(workingDirectories[stripe % workingDirectories.length], String.valueOf(versionedPartitionName.getPartitionVersion() % 1024));
+        return new File(workingDirectories[stripe % workingDirectories.length], String.valueOf(hash(versionedPartitionName) % 1024));
+    }
+
+    private final static long randMult = 0x5DEECE66DL;
+    private final static long randAdd = 0xBL;
+    private final static long randMask = (1L << 48) - 1;
+
+    private static long hash(VersionedPartitionName versionedPartitionName) {
+        if (versionedPartitionName.getPartitionName().isSystemPartition()) {
+            return versionedPartitionName.getPartitionVersion();
+        } else {
+            return hash(versionedPartitionName.getPartitionVersion());
+        }
+    }
+
+    private static long hash(long partitionVersion) {
+        long x = (partitionVersion * randMult + randAdd) & randMask;
+        long h = Math.abs(x >>> (16));
+        if (h >= 0) {
+            return h;
+        } else {
+            return Long.MAX_VALUE;
+        }
     }
 
     public <I extends WALIndex> WALStorage<I> create(
